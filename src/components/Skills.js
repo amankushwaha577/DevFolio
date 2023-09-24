b15a594a1e8739cfed5dc163f73d0883d6254987
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2023-09-24 22:43:49 +0530

    Library

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,6 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+import meter1 from "../assets/img/meter1.png";
+import meter2 from "../assets/img/meter2.png";
+import meter3 from "../assets/img/meter3.png";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
@@ -36,21 +36,27 @@ export const Skills = () => {
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
                         <p>I have a strong skills set on both front-end and back-end technologies. 
-                          Till now I have developed more than 15 MERN stack industrial level project. 
-                          All These projects are available on my github profile you can check on these.
+                        <br></br>Frontend Technologies: React.js, JavaScript, TypeScript, Next.js, 
+                                Pixi.js, Vue.js, Bootstrap, Tailwind, Three.js, JQuery, Material-UI, 
+                                HTML5, CSS, JSON, XML, Figma, Interfaces, NPM and Yarn.
+                        <br></br>Backend Technologies: Node.js, Express.js and MongoDB.
+                        <br></br>API: Context API, Rapid API and Rest API.
+                        <br></br>State Management: Redux.js
+                        <br></br>Communication Library: Socket.io
+                        <br></br>Version Control System: GIT, SVN.
                           </p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>MERN Stack : MongoDB, Express.js, React.js, Node.js</h5>
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <h5>Tailwind, Material.UI, Javascript, Typescript, HTML, CSS and Bootstrap</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>JS Libraries: PIXI.js, Vue.js, Next.js and Three.js</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
