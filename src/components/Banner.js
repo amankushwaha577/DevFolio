8d138d4fef885cc80ce0b15cbf87624ba5027f6b
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2023-09-24 20:58:22 +0530

    Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -55,9 +55,9 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to my Portfolio</span>
+                <span className="tagline">Welcome To My Personal Profile</span>
                 <h1>{`Hi! I'm Aman Kushwaha`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <p>A highly professional Software Development Engineer with 2.1 years of industrial experience.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
